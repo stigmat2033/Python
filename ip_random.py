d598a6de08fe9b9fc37f4f89799a411bb39c6209
Denis <stigmat2033@mail.ru>
2016-05-17 22:46:31 +0300

    Добавлен ip_random.py

---
+++ ip_random.py
@@ -0,0 +1,12 @@
+import random
+ip_r=[1,2,3,4]
+mask=''
+mask_r=[1,2,3,4]
+for i in [0,1,2,3]:
+    ip_r[i]=random.randint(1,255)
+print(ip_r)
+zeros=random.randint(1,32)
+mask+=str(('1'*zeros)+('0'*(32-zeros)))
+for i in range(0,4):
+    mask_r[i]=int((mask[(8*i):((8*i)+8)]),2)
+print(mask_r)

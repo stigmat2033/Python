9eba87e9014bf5892a0d727e05d9439c58bfd2a6
Denis <stigmat2033@users.noreply.github.com>
2016-05-27 16:18:26 +0300

    Update tag.py

--- Scripts/tag.py
+++ Scripts/tag.py
@@ -3,16 +3,16 @@ tag1='init' #Тег с которого удаляем
 tag2='kde' #Тег который удаляем
 c=0 #Счётчик изменений
 file=open('pkgstates','r') #Открываем файл со статусами пакетов
-text=file.readlines()
-file.close
-for x in range(0,len(text)):
-	if text[x].startswith('User-Tags:'):
-		if text[x].find(tag1) != -1:
-			if text[x].find(tag2) != -1:
-				text[x].replace((' '+tag2),'')
-				c+=1
-text=''.join(text)
-file=open('pkgstates_new','w')
-file.write(text)
-file.close
-print('Total changes: '+str(c))
+text=file.readlines() # Считываем все строки из файла
+file.close # Отпускаем файл
+for x in range(0,len(text)): # Цикл должен обходить все строки файла
+	if text[x].startswith('User-Tags:'): # Если строка начинается с 'User-Tags:' то:
+		if text[x].find(tag1) != -1: # Если в строке есть содержимое переменной tag1 то:
+			if text[x].find(tag2) != -1: # Если в строке есть содержимое переменной tag2 то:
+				text[x].replace((' '+tag2),'') # Заменить содержимое строки равное tag2 на ничего
+				c+=1 # Счётчик изменений +1
+text=''.join(text) # Объединяем все строки
+file=open('pkgstates_new','w') # Открываем файл на запись
+file.write(text) # Записываем в файл переменную text
+file.close # Отпускаем файл
+print('Total changes: '+str(c)) # Сообщаем сколько всего строк изменили

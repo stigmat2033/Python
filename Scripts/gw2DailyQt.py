450aa917959a38a2e27e4bdd8fc5f871d152ff96
Denis <stigmat2033@users.noreply.github.com>
2016-11-09 18:51:02 +0400

    Update gw2DailyQt.py

--- Scripts/gw2DailyQt.py
+++ Scripts/gw2DailyQt.py
@@ -1,172 +1,183 @@
 import sys, urllib.request, json
-from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QVBoxLayout, QGridLayout, QScrollArea, QLabel, QProgressBar
-from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QVBoxLayout, QGridLayout, QScrollArea, QLabel, QProgressBar, QSizePolicy
+from PyQt5.QtCore import QThread, pyqtSignal, Qt, QObject
 from PyQt5.QtGui import QPixmap
 
+class eventObject(QObject):
+    signal = pyqtSignal(int)
+    def __init__(self, type, id):
+        super().__init__()
+        self.type = type
+        self.id = id
+        self.thread = loadThread(r'https://api.guildwars2.com/v2/achievements?ids='+str(self.id))
+        self.thread.signal.connect(self.addData, Qt.QueuedConnection)
+        if self.type == 'pve':
+            self.thread2 = loadThread(r'https://wiki.guildwars2.com/images/c/ca/Explorer.png')
+        elif self.type == 'wvw':
+            self.thread2 = loadThread(r'https://wiki.guildwars2.com/images/d/d2/World_vs_World.png')
+        elif self.type == 'pvp':
+            self.thread2 = loadThread(r'https://wiki.guildwars2.com/images/c/ca/PvP_Conqueror.png')
+        else:
+            self.thread2 = loadThread(r'https://wiki.guildwars2.com/images/e/e1/Hall_of_Monuments_%28achievement%29.png')
+        self.thread2.signal.connect(self.addImage, Qt.QueuedConnection)
+        self.thread3 = loadThread(r'https://wiki.guildwars2.com/images/thumb/6/6b/Arenanet_Points.png/20px-Arenanet_Points.png')
+        self.thread3.signal.connect(self.addPic, Qt.QueuedConnection)
+
+    def start(self):
+        self.thread.start()
+        self.thread2.start()
+        self.thread3.start()
+
+    def addData(self, signal):
+        self.text = json.loads(signal.decode('UTF-8'))
+        self.signal.emit(1)
+
+    def addImage(self, signal):
+        self.pix = signal
+        self.signal.emit(1)
+
+    def addPic(self, signal):
+        self.pic = signal
+        self.signal.emit(1)
+
 class loadThread(QThread):
-    signal = pyqtSignal(bytes,str)
-    def __init__(self, url, arg=''):
+    signal = pyqtSignal(bytes)
+    def __init__(self, url):
         super().__init__()
         self.url = url
-        self.arg = str(arg)
 
     def run(self):
-        self.signal.emit(urllib.request.urlopen(self.url).read(),self.arg)
+        self.signal.emit(urllib.request.urlopen(self.url).read())
 
 class frame(QFrame):
-    def __init__(self, pix, name, reward, pix2, num):
+    def __init__(self,name, pix, pix2, reward):
         super().__init__()
-        # self.resize(60,40)
         self.grid = QGridLayout()
         self.grid.setSpacing(3)
 
         self.logo = QLabel()
-        self.logo.setPixmap(pix)
+        self.pix = QPixmap()
+        self.pix.loadFromData(pix)
+        self.logo.setPixmap(self.pix)
 
         self.name = QLabel()
+        if len(name) > 25:
+            name = (name[:25]+'...')
         self.name.setText(name)
 
         self.reward = QLabel()
         self.reward.setText(reward)
 
         self.pic = QLabel()
-        self.pic.setPixmap(pix2)
+        self.pix2 = QPixmap()
+        self.pix2.loadFromData(pix2)
+        self.pic.setPixmap(self.pix2)
 
-        self.num = num
-
-        self.grid.addWidget(self.logo,1,1,3,1)
-        self.grid.addWidget(self.name,1,2,2,1)
+        self.grid.addWidget(self.logo,2,1,3,1)
+        self.grid.addWidget(self.name,1,1,1,3)
         self.grid.addWidget(self.reward,2,2,2,1, Qt.AlignRight)
         self.grid.addWidget(self.pic,2,3,2,1)
         self.setLayout(self.grid)
 
 class mainFrame(QFrame):
-    def __init__(self, data, pix):
+    def __init__(self,data):
         super().__init__()
         self.data = data
-        self.pix = pix
 
         self.grid = QGridLayout()
         self.grid.setSpacing(3)
 
-        self.initFrames()
+        self.X = 1
+        self.Y = 1
 
-    def initFrames(self):
-        self.threads = []
-        x, y = 1, 1
-        for metaEvent in self.data.keys():
-            for event in self.data[metaEvent]:
-                setattr(self,'form{}'.format(str(event['id'])),frame(self.pix,'Event loading...','0',self.pix2))
-                if x == 4:
-                    x = 1
-                    y += 1
-                self.grid.addWidget(getattr(self,'form{}'.format(event['id'])),y,x,1,1)
-                x += 1
-                self.threads.append()
+        for i in self.data.keys():
+            setattr(self,'form{}'.format(str(i)),frame(
+                self.data[i].text[0]['name'],self.data[i].pix,self.data[i].pic,'0'
+            ))
+            if self.X == 4:
+                self.X = 1
+                self.Y += 1
+            self.grid.addWidget(
+                getattr(self,'form{}'.format(str(i))),self.Y,self.X,1,1
+            )
+            self.X += 1
         self.setLayout(self.grid)
 
 class window(QWidget):
-    def __init__(self):
+    def __init__(self,data):
         super().__init__()
+        self.data = data
+
         self.area = QScrollArea()
         self.vbox = QVBoxLayout()
 
-        self.done = 0
+        self.mainFrame = mainFrame(self.data)
 
-        self.threads = []
-        self.threads.append(loadThread(r'https://api.guildwars2.com/v2/achievements/daily','events'))
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/c/ca/Explorer.png','pve'))
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/c/ca/PvP_Conqueror.png','pvp'))
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/d/d2/World_vs_World.png','wvw'))
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/e/e1/Hall_of_Monuments_%28achievement%29.png','special'))
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/thumb/6/6b/Arenanet_Points.png/20px-Arenanet_Points.png','point'))
-        for i in self.threads:
-            i.signal.connect(self.getData, Qt.QueuedConnection)
-            i.start()
+        self.area.setWidget(self.mainFrame)
+        self.area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.pref = QSizePolicy()
+        self.pref.setHorizontalPolicy(QSizePolicy.Maximum)
+        self.area.setSizePolicy(self.pref)
+        self.vbox.addWidget(self.area)
+        self.setLayout(self.vbox)
+        self.show()
 
-        # self.thread = loadThread(r'https://api.guildwars2.com/v2/achievements/daily')
-        # self.thread.signal.connect(self.getData, Qt.QueuedConnection)
-        # self.thread.start()
+class launcher(QWidget):
+    signal = pyqtSignal(dict)
+    def __init__(self):
+        super().__init__()
+        self.vbox = QVBoxLayout()
 
-        # self.thread2 = loadThread(r'https://wiki.guildwars2.com/images/1/14/Daily_Achievement.png')
-        # self.thread2.signal.connect(self.getImage, Qt.QueuedConnection)
-        # self.thread2.start()
+        self.bar = QProgressBar()
+        self.bar.setMaximum(5)
 
-        # self.thread3 = loadThread(r'https://wiki.guildwars2.com/images/thumb/6/6b/Arenanet_Points.png/20px-Arenanet_Points.png')
-        # self.thread3.signal.connect(self.getImage2, Qt.QueuedConnection)
-        # self.thread3.start()
+        self.setWindowTitle('Loading data')
+        self.vbox.addWidget(self.bar)
+        self.setLayout(self.vbox)
 
-    def ready(self):
-        self.done += 1
-        if self.done == 5:
-            print('setting up main frame')
-            self.mainFrame = mainFrame(self.data, self.pix)
-            self.area.setWidget(self.mainFrame)
-            self.vbox.addWidget(self.area)
-            self.setLayout(self.vbox)
+        self.data = {}
+        self.total = 0
+        self.value = 0
+        self.thread = loadThread(r'https://api.guildwars2.com/v2/achievements/daily')
+        self.thread.signal.connect(self.getData, Qt.QueuedConnection)
+        self.thread.start()
 
-    def getData(self, data, flag):
-        pass
+    def sendData(self):
+        self.signal.emit(self.data)
+        self.close()
+
+    def progress(self):
+        self.value += 1
+        self.bar.setValue(self.value)
+        if self.bar.value() == self.total:
+            self.sendData()
+
+    def getData(self, signal):
+        items = json.loads(signal.decode('UTF-8'))
+        for metaEvent in items.keys():
+            for event in items[metaEvent]:
+                self.total += 3
+                self.data.setdefault(event['id'], eventObject(metaEvent, event['id']))
+                self.data[event['id']].signal.connect(self.progress)
+
+        self.bar.setMaximum(self.total)
+        for i in self.data.keys():
+            self.data[i].start()
 
 class application(QApplication):
     def __init__(self):
         super().__init__(sys.argv)
-        self.window = window()
+        self.launcher = launcher()
+        self.launcher.signal.connect(self.setData, Qt.QueuedConnection)
+        self.launcher.show()
+        self.exit(self.exec_())
+        self.window = window(self.data)
         self.window.show()
         self.exit(self.exec_())
 
-class launcherWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.vbox = QVBoxLayout()
-
-        self.status = QLabel()
-        self.bar = QProgressBar()
-
-        self.vbox.addWidget(self.status)
-        self.vbox.addWidget(self.bar)
-        self.setLayout(self.vbox)
-
-        self.total = 0
-        self.data = {}
-        self.threads = []
-
-        self.thread = loadThread(r'https://api.guildwars2.com/v2/achievements/daily')
-        self.thread.signal.connect(self.getData, Qt.QueuedConnection)
-        self.thread.start()
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/c/ca/Explorer.png','pve'))
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/e/e1/Hall_of_Monuments_%28achievement%29.png','special'))
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/c/ca/PvP_Conqueror.png','pvp'))
-        self.threads.append(loadThread(r'https://wiki.guildwars2.com/images/d/d2/World_vs_World.png','wvw'))
-
-        for i in self.threads:
-            i.signal.connect(self.addData, Qt.QueuedConnection)
-            i.start()
-
-    def addData(self, signal, flag):
-        print(flag)
-        self.data.setdefault(flag, signal)
-        self.total += 1
-        print(str(self.total))
-
-    def getData(self, signal, flag):
-        items = json.loads(signal.decode('UTF-8'))
-        print(items)
-        ids = ''
-        for metaEvent in items.keys():
-            for event in items[metaEvent]:
-                ids = ids + str(event['id']) + ','
-        self.thread = loadThread(r'https://api.guildwars2.com/v2/achievements?ids='+ids.rstrip(','),'daily')
-        self.thread.signal.connect(self.addData, Qt.QueuedConnection)
-        self.thread.start()
-
-class launcher(QApplication):
-    def __init__(self):
-        super().__init__(sys.argv)
-        self.window = launcherWindow()
-        self.window.show()
-        self.exit(self.exec_())
+    def setData(self, signal):
+        self.data = signal
 
 if __name__ == '__main__':
-    data = launcher()
-    # app = application()
+    app = application()
+    print(app)
